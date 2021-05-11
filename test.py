1d1b139ab0caee78bd09e8d8aba33ac7db71ebb0
StephanieR34 <77360915+StephanieR34@users.noreply.github.com>
2021-05-11 16:07:30 +0200

    effacer les commentaire inutile

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
 import unittest
 from parse_log import readlog, temps_minutes, total_time, regroupement, somme, change_values, pourcentage
 
-# v= """
-# 11:00-11:30 Exercises
-# 11:30-12:00 Break
-# """
 
-
-# r="""
-# Break                     30 minutes    50%
-# Exercises                 30 minutes    50%
-# """
-# class TestApp(unittest.TestCase):
-#     def test_parse_log(self):
-#         #TODO change functiun's name
-#         self.assertEqual(toto(v), r)
-#         self.assertEqual(toto(v)[-1], "%")
-#         self.assertEqual(len(toto(v),43)
 
 class TestApp(unittest.TestCase):
     def test_readlog(self):
